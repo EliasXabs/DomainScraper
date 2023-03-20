9bd62a720e52ef3d436c13b940494871010a7ebc
EliasXabs <EliasXabs@gmail.com>
2023-03-20 09:30:48 +0200

    Added subdomains

--- scraper.py
+++ scraper.py
@@ -4,3 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("Domain_name", help="Name of the domain that will be sraped")
 args = parser.parse_args()
+
+# Note use args.print_string() to print the argument content
+
+
