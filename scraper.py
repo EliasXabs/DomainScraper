70c7791e8a823a36a2dac0ceae2762e417deb917
EliasXabs <EliasXabs@gmail.com>
2023-03-20 10:40:54 +0200

    Added a verifier to check the domain

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
 import re
 import argparse
+import requests
 
 parser = argparse.ArgumentParser()
-parser.add_argument("Domain_name", help="Name of the domain that will be sraped")
-args = parser.parse_args()
+parser.add_argument("Domain_name", help="Name of the domain that will be scraped")
+domain = getattr(parser.parse_args(), "Domain_name")
 
-# Note use args.print_string() to print the argument content
+exists = requests.get('http://www.google.com')
 
-
+if exists :
+    print("Exists")
+else :
+    print("Does not exist")
