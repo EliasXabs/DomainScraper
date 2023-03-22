f80ee2ab73109483f40b46005474c3c5d452f8ad
EliasXabs <EliasXabs@gmail.com>
2023-03-22 11:10:16 +0200

    Validates protocol

--- scraper.py
+++ scraper.py
@@ -4,13 +4,25 @@ import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Domain_name", help="Name of the domain that will be scraped")
-domain = getattr(parser.parse_args(), "Domain_name")
+try :
+    domain = getattr(parser.parse_args(), "Domain_name")
+except :
+    print ("Please enter a valid domain") 
+    exit()
 
-exists = requests.get('http://www.google.com')
+if not re.match(r"^https?:\/\/", domain) :
+    sec = input("Is the protocol secure? (y/n): ")
+    if sec.lower() == "y" :
+        domain = "https://"+domain
+    elif sec.lower == "n" :
+        domain = "http://"+domain
+    else:
+        print("Please enter a valid answer")
+
+exists = requests.get(domain)
 
 if exists :
     print("Exists")
 else :
     print("Error : Please enter a domain that exists")
-    print("Terminating . . .")
     exit()
